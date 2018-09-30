2a5bc34fcdba861d9ae949a353ecf0ef97c2fdb9
nydi <stefan.nydegger@gmail.com>
2018-09-30 09:57:26 +0200

    working firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AngularFirestore} from '@angular/fire/firestore';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'firestoretest';
+  items: any;
+
+  constructor(private db: AngularFirestore) {
+  }
+
+  ngOnInit(): void {
+    this.items = this.db.collection('availableExercises').valueChanges().subscribe(value => {
+      console.log('value:', value);
+    });
+  }
 }
